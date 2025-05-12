7eb2c8c26970937823b939bd4f575ce3056f371c
Theresa Abl <93684898+theresaabl@users.noreply.github.com>
2025-05-12 15:50:34 +0000

    Add project hyptheses and validation page to dashboard

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -4,4 +4,27 @@ def page_project_hypothesis_body():
 
     st.write("### Project Hypotheses and Validation")
 
-    st.info("This section contains info about the project hypotheses and validation")
+    st.info(
+        "### Hypotheses\n"
+        "1. We expect that high-risk patients tend to have high blood sugar "
+        "levels.\n"
+        "2. We expect that high-risk patients tend to have high systolic blood "
+        "pressure levels.\n"
+        "3. We expect that high-risk patients tend to have high diastolic "
+        "blood pressure levels.\n"
+        "4. We expect that high-risk patients tend to be of a higher age."
+        )
+    
+    st.success(
+        "### Validation\n"
+        " * The correlation study on the Maternal Health Risk Study page "
+        "supports all four hypotheses.\n"
+        "* From the correlation levels of the variables with the health risk "
+        "level, we conclude that:\n"
+        "\n"
+        "1. High-risk patients tend to have high blood sugar "
+        "levels.\n"
+        "2. High-risk patients tend to have high systolic and diastolic blood "
+        "pressure levels, where the two are correlated between themselves.\n"
+        "3. High-risk patients tend to be of a higher age."
+        )
--- src/machine_learning/create_plots.py
+++ src/machine_learning/create_plots.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from feature_engine.discretisation import ArbitraryDiscretiser
 import plotly.express as px
 
